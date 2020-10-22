c5e02863c306e07f5af1a1b01b5bf1e91a82ade4
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-22 08:34:54 -0600

    added student constructor

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public void Student(long studentID, String studentName){
+        this.id = studentID;
+        this.name = studentName;
+        this.grades = new ArrayList<>();
+    }
+
 }
